b740ce7e2a0872db90d5164926198769a82b9faf
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-18 12:26:36 +0100

    fixing syntax erro

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 # a 2 digit combinator programm
 
 for i in range(0, 9):
     for n in range(i+1, 10):
         if(i<8):
-            print("{}{}, ".format(i, n), end='')
+            print("{}{},".format(i, n), end=' ')
         if(i>=8):
             print("{}{}".format(i, n), end='\n')
