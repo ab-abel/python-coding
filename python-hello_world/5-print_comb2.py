0d0a0116714467ed0f23677ec145803d3e70add3
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-18 12:15:48 +0100

    resolved syntax error

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,7 @@
+#!/usr/bin/python3
 # a program that  numbers from 0 to 99 
-
 for i in range(0, 100):
     if(i<99):
-        print("{:02d}, ".format(i), end='')
+        print("{:02d},".format(i), end=' ')
     elif(i==99):
-        print("{} ".format(i), end='\n')
+        print("{}".format(i), end='\n')
